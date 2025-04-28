c86643a8ed62c9b2a0cb08cba054d380c4dd340b
Delta-Pion <adi.28per@gmail.com>
2025-04-28 20:03:34 +0530

    <C-l> Removed to clear terminal
    I found that in terminal mode <C-;> also escapes the insert to gonna use
    that for left window switch as I rarely use it

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,8 +11,13 @@ vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { noremap = true })
 vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { noremap = true })
 vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { noremap = true })
 vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { noremap = true })
-vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { noremap = true })
+-- vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { noremap = true })
+-- instead use <C-;> for esc and <C-l> to shift because this motion is quite rare
+-- This saves <C-l> for clearing the screen
+
+-- New Escape mapping
 vim.keymap.set("i", "kj", "<ESC>", { noremap = true })
+
 -- Window Switching Normal
 --:noremap <C-h> <C-w>h
 --:noremap <C-j> <C-w>j
